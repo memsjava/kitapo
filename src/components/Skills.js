266487ad33067f337685060addc12f882869e7cf
RABEMAMPIANDRA Eric <memsjava@gmail.com>
2022-12-26 13:50:41 +0300

    update 26 dec

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,14 +1,7 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter2.svg";
 
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import colorSharp from "../assets/img/color-sharp.png"
-import DoughNut from "../assets/charts/Donut";
-import DoughNut3 from "../assets/charts/Donut3";
-import DoughNut2 from "../assets/charts/Donut2";
-import DoughNut1 from "../assets/charts/Donut1";
 
 export const Skills = () => {
   const responsive = {
@@ -31,6 +24,21 @@ export const Skills = () => {
     }
   };
 
+  const data ={
+        "total_achat": 0,
+        "total_retrait": 0,
+        "total_buy":0,
+        "total_sell":0,
+        "ETH": 0.0,
+        "USDT":0.00,
+        "BTC":0.00
+    }
+  const cours = {
+    "ETH": 1600,
+    "USDT": 4200,
+    "BTC":15600
+  }
+
   return (
     <section className="skill" id="skills">
         <div className="container">
@@ -38,27 +46,98 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Antontan'isa</h2>
-                        <p>Ny taremarika ho hitantsika manaraka eto dia nalaina tao amin'ny compte'ko. Izany hoe tena izy io fa tsy demo akory.<br></br></p>
+                        <p>Ny taremarika ho hitantsika manaraka eto dia nalaina tao amin'ny compte'ko.<br></br></p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={meter1} alt="boby napedina"/>
+                                {/* <img src={meter1} alt="boby napedina"/> */}
                                 {/* <DoughNut /> */}
+                                <svg width="200" height="200">
+                                  <circle cx="100" cy="100" r="60" stroke-width="20" strokeDasharray="330" stroke="url(#gradient)" fill="transparent"/>
+                                  <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="0" stop-color="#800080"/>
+                                    <stop offset="1" stop-color="#00F"/>
+                                  </linearGradient>
+                                  <g className="donut-text donut-text-1">
+                                  <text y="50%" transform="translate(0, 2)">
+                                    <tspan x="50%" textAnchor="middle" className="donut-percent">{data['total_achat']- data['total_retrait']}</tspan>   
+                                  </text>
+                                  <text y="60%" transform="translate(0, 2)">
+                                    <tspan x="50%" textAnchor="middle" className="donut-data">Ar</tspan>   
+                                  </text>
+                                  </g>
+                                </svg>
                                 <h5>Vola nampiasaina</h5>
                             </div>
-                            <div className="item" alt="fitsinjarana">
-                                <img src={meter2} />
-                                {/* <DoughNut1 /> */}
-                                <h5>Fitsinjarana BTC / USDT</h5>
-                            </div>
+                            
                             <div className="item">
                                 {/* <DoughNut3 /> */}
-                                <img src={meter2} />
+                                {/* <img src={meter2} /> */}
+                                <svg width="200" height="200">
+                                  <circle cx="100" cy="100" r="60" stroke-width="20" stroke-dasharray="330" stroke="url(#gradient)" fill="transparent"/>
+                                  <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
+                                  <stop offset="0" stop-color="#800080"/>
+                                  <stop offset="1" stop-color="#00F"/>
+
+                                  </linearGradient>
+                                  <g className="donut-text donut-text-1">
+                                  <text y="50%" transform="translate(0, 2)">
+                                    <tspan x="50%" textAnchor="middle" className="donut-percent">
+                                      {
+                                        data['BTC']*cours['BTC']*cours['USDT'] + data['ETH']*cours['ETH']*cours['USDT'] + data['USDT']*cours['USDT']
+                                      }
+                                    
+                                    </tspan>   
+                                  </text>
+                                  <text y="60%" transform="translate(0, 2)">
+                                    <tspan x="50%" textAnchor="middle" className="donut-data">Ar</tspan>   
+                                  </text>
+                                  </g>
+                                </svg>
                                 <h5>Sandany anio</h5>
                             </div>
                             <div className="item">
-                            <img src={meter2} />
+                            {/* <img src={meter2} /> */}
                               {/* <DoughNut2 /> */}
+                              <svg width="200" height="200">
+                                  <circle cx="100" cy="100" r="60" stroke-width="20" strokeDasharray="330" stroke="url(#gradient)" fill="transparent"/>
+                                  <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="0" stop-color="#800080"/>
+                                    <stop offset="1" stop-color="#00F"/>
+                                  </linearGradient>
+                                  <g className="donut-text donut-text-1">
+                                  <text y="50%" transform="translate(0, 2)">
+                                    <tspan x="50%" textAnchor="middle" className="donut-percent"> 
+                                    {
+                                      
+                                      ((-(data['total_achat']- data['total_retrait']) + (data['BTC']*cours['BTC']*cours['USDT'] + data['ETH']*cours['ETH']*cours['USDT'] + data['USDT']*cours['USDT'] )) / (data['total_achat']- data['total_retrait'])  * 100 ).toFixed(2) 
+                                    
+                                    }
+                                    </tspan>   
+                                  </text>
+                                  <text y="60%" transform="translate(0, 2)">
+                                    <tspan x="50%" textAnchor="middle" className="donut-data">%</tspan>   
+                                  </text>
+                                  </g>
+                                </svg>
                               <h5>ROI </h5>
+                            </div>
+                            <div className="item" alt="fitsinjarana">
+                                <svg width="200" height="200">
+                                  <circle cx="50" cy="50" 
+                                  r= {data['BTC']*cours['BTC']*cours['USDT']  * 50 / (data['BTC']*cours['BTC']*cours['USDT'] + data['ETH']*cours['ETH']*cours['USDT'] + data['USDT']*cours['USDT'])} 
+                                  fill="#800080"/>
+                                  <text x="50" y="50" text-anchor="middle" dominant-baseline="central" font-size="0.7em" fill="#00FFFF">{data['BTC']} BTC</text>
+                                  <circle cx="150" cy="50"
+                                      r= {(data['ETH']*cours['ETH']*cours['USDT']  * 50 / (data['BTC']*cours['BTC']*cours['USDT'] + data['ETH']*cours['ETH']*cours['USDT'] + data['USDT']*cours['USDT'])).toString()} 
+
+                                   fill="url(#gradient)"/>
+                                  <text x="150" y="50" text-anchor="middle" dominant-baseline="central" font-size="0.7em"  fill="#00FFFF">{data['ETH']} ETH</text>
+                                  <circle cx="100" cy="150" r= {data['USDT']*cours['USDT']  * 50 / (data['BTC']*cours['BTC']*cours['USDT'] + data['ETH']*cours['ETH']*cours['USDT'] + data['USDT']*cours['USDT'])} 
+
+                                  fill="#00F"/>
+                                  <text x="100" y="150" text-anchor="middle" dominant-baseline="central"font-size="0.7em" fill="#00FFFF">{data['USDT']} USDT</text>
+                                </svg>
+                                <h5>Fitsinjarana BTC / ETH / USDT</h5>
                             </div>
                         </Carousel>
                     </div>
